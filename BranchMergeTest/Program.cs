0be301a5a93f1681f7c835362db4ac1d5bb5c8bf
alex <acrang@gmail.com>
2020-10-29 10:46:18 -0400

    First commit for the Branch Merge Test

---
+++ BranchMergeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BranchMergeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test file to try merging branches on GitHub!");
+        }
+    }
+}
